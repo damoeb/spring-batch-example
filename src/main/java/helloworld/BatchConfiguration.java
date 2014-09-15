a72a7e13bda4caf977480efd336d0de0ac987f97
damoeb <markus@migor.org>
2014-09-15 18:47:32 +0200

    Batch using builders

--- src/main/java/helloworld/BatchConfiguration.java
+++ src/main/java/helloworld/BatchConfiguration.java
@@ -1,31 +1,28 @@
 package helloworld;
 
 import org.springframework.batch.core.*;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.job.AbstractJob;
-import org.springframework.batch.core.job.SimpleJob;
 import org.springframework.batch.core.job.flow.FlowExecutionStatus;
 import org.springframework.batch.core.job.flow.FlowJob;
 import org.springframework.batch.core.job.flow.JobExecutionDecider;
-import org.springframework.batch.core.job.flow.support.SimpleFlow;
-import org.springframework.batch.core.job.flow.support.StateTransition;
-import org.springframework.batch.core.job.flow.support.state.DecisionState;
-import org.springframework.batch.core.job.flow.support.state.EndState;
-import org.springframework.batch.core.job.flow.support.state.StepState;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.launch.support.SimpleJobLauncher;
 import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
 import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
 import org.springframework.batch.core.repository.JobRestartException;
 import org.springframework.batch.core.repository.dao.*;
 import org.springframework.batch.core.repository.support.SimpleJobRepository;
-import org.springframework.batch.core.step.tasklet.TaskletStep;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.batch.support.transaction.ResourcelessTransactionManager;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.LinkedList;
-import java.util.List;
+import org.springframework.core.task.TaskExecutor;
 
 /**
  * A basic spring batch example for SimpleJob and FlowJob
@@ -36,6 +33,17 @@ import java.util.List;
  */
 @Configuration
 public class BatchConfiguration {
+
+    public static final FlowExecutionStatus BLOCKED = new FlowExecutionStatus("BLOCKED");
+    public static final FlowExecutionStatus YES = new FlowExecutionStatus("YES");
+    public static final FlowExecutionStatus NO = new FlowExecutionStatus("NO");
+    public static final FlowExecutionStatus VITAL = new FlowExecutionStatus("VITAL");
+
+
+    private boolean vital = true;
+    private boolean blocked = false;
+    private boolean running = false;
+
 
     public static void main(String[] args) throws JobExecutionAlreadyRunningException, JobRestartException, JobInstanceAlreadyCompleteException, JobParametersInvalidException {
         ApplicationContext context = new AnnotationConfigApplicationContext(BatchConfiguration.class);
@@ -61,45 +69,171 @@ public class BatchConfiguration {
     }
 
     @Bean
-    public FlowJob getFlowJob() {
-        FlowJob job = new FlowJob();
+    public Job getFlowJob() {
+        return getJobBuilderFactory()
+                .get("a")
+                .incrementer(new RunIdIncrementer())
+                .flow(getInitState())
+                .next(decide_environmentVitality())
+                .on(c(VITAL))
+                .to(decide_hatLaufendeAlarmierungen())
+                .on(c(NO))
+                .to(decide_hatBlockierteDaten())
+                .on(c(YES))
+                .end()
+                .on(c(NO))
+                .to(do_StarteStammdatenlieferung())
+                .next(do_DatenSelektieren())
+                .next(do_DatenAnreichern())
+                .next(do_InDbPersistieren())
+//                .split(split_Processing())
+                .end()
+                .build();
+    }
 
-        job.setFlow(getMainFlow());
+    @Bean
+    public TaskExecutor split_Processing() {
+        return new TaskExecutor() {
+            @Override
+            public void execute(Runnable task) {
 
-        job.setJobRepository(getJobRepository());
+            }
+        };
+    }
 
-        return job;
+    private String c(FlowExecutionStatus executionStatus) {
+        return executionStatus.getName();
+    }
+
+    @Bean
+    public JobExecutionDecider decide_environmentVitality() {
+        return new JobExecutionDecider() {
+
+            @Override
+            public FlowExecutionStatus decide(JobExecution jobExecution, StepExecution stepExecution) {
+
+                System.out.println("Vitality? " + vital);
+
+                if(vital) {
+                    return VITAL;
+                } else {
+                    return FlowExecutionStatus.FAILED;
+                }
+            }
+        };
+    }
+
+    @Bean
+    public JobExecutionDecider decide_hatBlockierteDaten() {
+        return new JobExecutionDecider() {
+
+            @Override
+            public FlowExecutionStatus decide(JobExecution jobExecution, StepExecution stepExecution) {
+
+                System.out.println("Blocked data? " + blocked);
+
+                if(blocked) {
+                    return YES;
+                } else {
+                    return NO;
+                }
+            }
+        };
+    }
+
+    @Bean
+    public JobExecutionDecider decide_hatLaufendeAlarmierungen() {
+        return new JobExecutionDecider() {
+
+            @Override
+            public FlowExecutionStatus decide(JobExecution jobExecution, StepExecution stepExecution) {
+
+                System.out.println("Any Running? " + running);
+
+//                return running ? FlowExecutionStatus.STOPPED : FlowExecutionStatus.COMPLETED;
+                if(running) {
+                    return YES;
+                } else {
+                    return NO;
+                }
+
+            }
+        };
     }
 
 
     @Bean
-    public SimpleJob getSimpleJob() {
-        SimpleJob job = new SimpleJob();
-
-        job.addStep(getStep1());
-        job.addStep(getStep2());
-
-        job.setJobRepository(getJobRepository());
-
-        return job;
+    public Step getInitState() {
+        return getStepBuilderFactory()
+                .get("step1")
+                .tasklet(new Tasklet() {
+                    @Override
+                    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+                        System.out.println("Initialize");
+                        return RepeatStatus.FINISHED;
+                    }
+                })
+                .build();
     }
 
     @Bean
-    public Step getStep1() {
-        TaskletStep step = new TaskletStep();
-        step.setTasklet(new PrintTasklet("Hello"));
-        step.setJobRepository(getJobRepository());
-        step.setTransactionManager(getTransactionManager());
-        return step;
+    public Step do_InDbPersistieren() {
+        return getStepBuilderFactory()
+                .get("do_InDbPersistieren")
+                .tasklet(new Tasklet() {
+
+                    @Override
+                    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+                        System.out.println("Daten in DB persistieren");
+                        return RepeatStatus.FINISHED;
+                    }
+                })
+                .build();
     }
 
     @Bean
-    public Step getStep2() {
-        TaskletStep step = new TaskletStep();
-        step.setTasklet(new PrintTasklet("World"));
-        step.setJobRepository(getJobRepository());
-        step.setTransactionManager(getTransactionManager());
-        return step;
+    public Step do_DatenAnreichern() {
+        return getStepBuilderFactory()
+                .get("do_DatenAnreichern")
+                .tasklet(new Tasklet() {
+
+                    @Override
+                    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+                        System.out.println("Daten anreichern");
+                        return RepeatStatus.FINISHED;
+                    }
+                })
+                .build();
+    }
+
+    @Bean
+    public Step do_DatenSelektieren() {
+        return getStepBuilderFactory()
+                .get("do_DatenSelektieren")
+                .tasklet(new Tasklet() {
+
+                    @Override
+                    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+                        System.out.println("Daten selektieren");
+                        return RepeatStatus.FINISHED;
+                    }
+                })
+                .build();
+    }
+
+    @Bean
+    public Step do_StarteStammdatenlieferung() {
+        return getStepBuilderFactory()
+                .get("ftpUpload")
+                .tasklet(new Tasklet() {
+
+                    @Override
+                    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+                        System.out.println("Starte Stammdatenlieferung");
+                        return RepeatStatus.FINISHED;
+                    }
+                })
+                .build();
     }
 
     @Bean
@@ -127,29 +261,34 @@ public class BatchConfiguration {
         return new MapExecutionContextDao();
     }
 
-    public SimpleFlow getMainFlow() {
-        SimpleFlow flow = new SimpleFlow("main");
-        List<StateTransition> transitions = new LinkedList<StateTransition>();
-
-        transitions.add(StateTransition.createStateTransition(new StepState("1", getStep1()), "2"));
-        transitions.add(StateTransition.createStateTransition(new DecisionState(getDecider(), "d"), "2"));
-        transitions.add(StateTransition.createStateTransition(new StepState("2", getStep2()), "end"));
-        transitions.add(StateTransition.createEndStateTransition(new EndState(FlowExecutionStatus.COMPLETED, "end")));
-
-        flow.setStateTransitions(transitions);
-
-        return flow;
-    }
-
     public JobExecutionDecider getDecider() {
         return new JobExecutionDecider() {
 
             @Override
             public FlowExecutionStatus decide(JobExecution jobExecution, StepExecution stepExecution) {
-                stepExecution.getExitStatus();
-                return FlowExecutionStatus.STOPPED;
+                boolean left = true;
+
+                if(left) {
+                    jobExecution.setExitStatus(ExitStatus.COMPLETED);
+//                    return FlowExecutionStatus.COMPLETED;
+                } else {
+                    jobExecution.setExitStatus(ExitStatus.EXECUTING);
+                }
+                return FlowExecutionStatus.COMPLETED;
+
             }
         };
     }
+
+    @Bean
+    public JobBuilderFactory getJobBuilderFactory() {
+        return new JobBuilderFactory(getJobRepository());
+    }
+
+    @Bean
+    public StepBuilderFactory getStepBuilderFactory() {
+        return new StepBuilderFactory(getJobRepository(), getTransactionManager());
+    }
+
 }
 
